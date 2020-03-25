b1c308c588e23d90f0bf34d81af9d1945dca6d83
Andreas Kanz <andreas@akanz.de>
2020-03-25 12:59:14 +0100

    initial commit

---
+++ klib/__init__.py
@@ -0,0 +1,12 @@
+"""
+Data Science Module for Python
+==================================
+klib is a Python module ... ...
+"""
+__all__ = ['clean', 'describe', 'ingest']
+
+print('test init')
+
+import scrap_file
+
+scrap_file.main()
---
+++ klib/scrap_file.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn import model_selection
+import sklearn
+
+
+sklearn.model_selection.GridSearchCV
+
+model_selection.GridSearchCV
+
+GridSearchCV
+
+print('when not main')
+
+def main():
+    print('main')
+
+if __name__ == "__main__":
+    main()
+
