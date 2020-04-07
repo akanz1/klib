7d5e134e487a9d3761c6374fda9268d00d648704
Andreas Kanz <Andreas@akanz.de>
2020-04-07 17:35:06 +0200

    -

--- klib/__init__.py
+++ klib/__init__.py
@@ -7,9 +7,9 @@ klib is a Python module ... ...
 __author__ = """Andreas Kanz"""
 
 from ._version import __version__
-from .describe import corr_plot, missingval_plot
+from .describe import corr_mat, corr_plot, missingval_plot
 from .clean import convert_datatypes, data_cleaning, drop_missing
 
 __version__ = __version__
 
-__all__ = ['corr_plot', 'missingval_plot', 'convert_datatypes', 'data_cleaning', 'drop_missing']
+__all__ = ['convert_datatypes', 'corr_mat', 'corr_plot', 'data_cleaning', 'drop_missing', 'missingval_plot']
