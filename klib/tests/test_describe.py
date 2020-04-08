60150937ca2f195feb6b5ff464d31cc8487d882a
Andreas Kanz <Andreas@akanz.de>
2020-04-08 12:45:37 +0200

    loop over repetitive test cases

--- klib/tests/test_describe.py
+++ klib/tests/test_describe.py
@@ -35,56 +35,48 @@ class Test__missing_vals(unittest.TestCase):
 
     def test_mv_rows(self):
         # Test missing values for each row
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows'][0], 1)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows'][1], 2)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows'][2], 1)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows'][3], 1)
+        expected_results = [1, 2, 1, 1]
+        for i, _ in enumerate(expected_results):
+            self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows'][i], expected_results[i])
 
     def test_mv_cols(self):
         # Test missing values for each column
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols'][0], 1)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols'][1], 1)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols'][2], 1)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols'][3], 2)
+        expected_results = [1, 1, 1, 2]
+        for i, _ in enumerate(expected_results):
+            self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols'][i], expected_results[i])
 
     def test_mv_rows_ratio(self):
         # Test missing values ratio for each row
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows_ratio'][0], 0.25)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows_ratio'][1], 0.5)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows_ratio'][2], 0.25)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows_ratio'][3], 0.25)
+        expected_results = [0.25, 0.5, 0.25, 0.25]
+        for i, _ in enumerate(expected_results):
+            self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_rows_ratio'][i], expected_results[i])
 
         # Test if missing value ratio is between 0 and 1
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_rows_ratio'][0] <= 1)
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_rows_ratio'][1] <= 1)
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_rows_ratio'][2] <= 1)
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_rows_ratio'][3] <= 1)
+        for i in range(len(self.data_mv_df)):
+            self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_rows_ratio'][i] <= 1)
 
     def test_mv_cols_ratio(self):
-        # Test missing values ratio for each row
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols_ratio'][0], 0.25)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols_ratio'][1], 0.25)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols_ratio'][2], 0.25)
-        self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols_ratio'][3], 0.5)
+        # Test missing values ratio for each column
+        expected_results = [1/4, 0.25, 0.25, 0.5]
+        for i, _ in enumerate(expected_results):
+            self.assertAlmostEqual(_missing_vals(self.data_mv_df)['mv_cols_ratio'][i], expected_results[i])
 
         # Test if missing value ratio is between 0 and 1
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_cols_ratio'][0] <= 1)
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_cols_ratio'][1] <= 1)
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_cols_ratio'][2] <= 1)
-        self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_cols_ratio'][3] <= 1)
+        for i in range(len(self.data_mv_df)):
+            self.assertTrue(0 <= _missing_vals(self.data_mv_df)['mv_cols_ratio'][i] <= 1)
 
 
 class Test_corr_mat(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.data_corr = pd.DataFrame([[1, 0, 3j, 4],
+        cls.data_corr = pd.DataFrame([[1, 0, 3, 4],
                                       [3, 4, 5, 6],
                                       ['a', 'b', pd.NA, 'd'],
                                       [5, False, np.nan, pd.NaT]],
                                      columns=['Col1', 'Col2', 'Col3', 'Col4'])
 
-        cls.data_corr_list = [1, 2, -3, 4j, 5, 0]
+        cls.data_corr_list = [1, 2, -3, 4, 5, 0]
 
     def test_output_type(self):
         # Test conversion from pd.io.formats.style.Styler to pd.core.frame.DataFrame
