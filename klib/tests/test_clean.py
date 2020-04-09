487c57861850f4dec78071afc1e0ed2851fbc30e
Andreas Kanz <Andreas@akanz.de>
2020-04-09 19:21:59 +0200

    formatting

---
+++ klib/tests/test_clean.py
@@ -0,0 +1,11 @@
+# import numpy as np
+# import pandas as pd
+import unittest
+# from klib.clean import convert_datatypes, drop_missing, data_cleaning
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class Test__missing_vals(unittest.TestCase):
+    pass
