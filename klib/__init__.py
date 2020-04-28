96b70c8712a9ad7207430f60378e03a4c5438dc7
Andreas Kanz <Andreas@akanz.de>
2020-04-28 16:32:55 +0200

    remove circular dependency

--- klib/__init__.py
+++ klib/__init__.py
@@ -15,12 +15,13 @@ from .preprocess import *
 
 __version__ = __version__
 
-# __all__ = ['cat_plot',
-#            'convert_datatypes',
-#            'corr_mat',
-#            'corr_plot',
-#            'data_cleaning',
-#            'dist_plot',
-#            'drop_missing',
-#            'missingval_plot',
-#            'train_dev_test_split']
+__all__ = ['cat_plot',
+           'convert_datatypes',
+           'corr_mat',
+           'corr_plot',
+           'data_cleaning',
+           'dist_plot',
+           'drop_missing',
+           'missingval_plot',
+           'mv_col_handling',
+           'train_dev_test_split']
--- klib/preprocess.py
+++ klib/preprocess.py
@@ -6,8 +6,6 @@ Functions for data preprocessing.
 '''
 
 # Imports
-from .describe import corr_mat
-from .utils import _missing_vals
 import numpy as np
 import pandas as pd
 
@@ -23,81 +21,10 @@ from .utils import _validate_input_int
 from .utils import _validate_input_range
 
 
-__all__ = ['mv_col_handler',
-           'train_dev_test_split',
+__all__ = ['train_dev_test_split',
            'cat_pipe',
            'num_pipe',
            'preprocessing_pipe']
-
-
-def mv_col_handler(data, target=None, mv_threshold=0.1, corr_thresh_features=0.6, corr_thresh_target=0.3):
-    '''
-    Converts columns with a high ratio of missing values into binary features and eventually drops them based on \
-    their correlation with other features and the target variable. This function follows a three step process:
-    - 1) Identify features with a high ratio of missing values
-    - 2) Identify high correlations of these features among themselves and with other features in the dataset.
-    - 3) Features with high ratio of missing values and high correlation among each other are dropped unless \
-         they correlate reasonably well with the target variable.
-
-    Note: If no target is provided, the process exits after step two and drops columns identified up to this point.
-
-    Parameters
-    ----------
-    data: 2D dataset that can be coerced into Pandas DataFrame.
-
-    target: string, list, np.array or pd.Series, default None
-        Specify target for correlation. E.g. label column to generate only the correlations between each feature \
-        and the label.
-
-    mv_threshold: float, default 0.1
-        Value between 0 <= threshold <= 1. Features with a missing-value-ratio larger than mv_threshold are candidates \
-        for dropping and undergo further analysis.
-
-    corr_thresh_features: float, default 0.6
-        Value between 0 <= threshold <= 1. Maximum correlation a previously identified features with a high mv-ratio is\
-         allowed to have with another feature. If this threshold is overstepped, the feature undergoes further analysis.
-
-    corr_thresh_target: float, default 0.3
-        Value between 0 <= threshold <= 1. Minimum required correlation of a remaining feature (i.e. feature with a \
-        high mv-ratio and high correlation to another existing feature) with the target. If this threshold is not met \
-        the feature is ultimately dropped.
-
-    Returns
-    -------
-    data: Updated Pandas DataFrame
-    cols_mv: Columns with missing values included in the analysis
-    drop_cols: List of dropped columns
-    '''
-
-    # Validate Inputs
-    _validate_input_range(mv_threshold, 'mv_threshold', 0, 1)
-    _validate_input_range(corr_thresh_features, 'corr_thresh_features', 0, 1)
-    _validate_input_range(corr_thresh_target, 'corr_thresh_target', 0, 1)
-
-    data = pd.DataFrame(data).copy()
-    data_local = data.copy()
-    mv_ratios = _missing_vals(data_local)['mv_cols_ratio']
-    cols_mv = mv_ratios[mv_ratios > mv_threshold].index.tolist()
-    data_local[cols_mv] = data_local[cols_mv].applymap(lambda x: 1 if not pd.isnull(x) else x).fillna(0)
-
-    high_corr_features = []
-    data_temp = data_local.copy()
-    for col in cols_mv:
-        corrmat = corr_mat(data_temp, colored=False)
-        if abs(corrmat[col]).nlargest(2)[1] > corr_thresh_features:
-            high_corr_features.append(col)
-            data_temp = data_temp.drop(columns=[col])
-
-    drop_cols = []
-    if target is None:
-        data = data.drop(columns=high_corr_features)
-    else:
-        for col in high_corr_features:
-            if pd.DataFrame(data_local[col]).corrwith(target)[0] < corr_thresh_target:
-                drop_cols.append(col)
-                data = data.drop(columns=[col])
-
-    return data, cols_mv, drop_cols
 
 
 def train_dev_test_split(data, target, dev_size=0.1, test_size=0.1, stratify=None, random_state=408):
@@ -224,26 +151,3 @@ def preprocessing_pipe(num=True, cat=True):
         pipe = cat_pipe()
 
     return pipe
-
-
-class MVColHandler(BaseEstimator, TransformerMixin):
-    '''possible component of a cleaning pipeline --> follows DataCleaning'''
-
-    def __init__(self, target=None, mch_mv_thresh=0.1, mch_feature_thresh=0.6, mch_target_thresh=0.3):
-        self.target = target
-        self.mch_mv_thresh = mch_mv_thresh
-        self.mch_feature_thresh = mch_feature_thresh
-        self.mch_target_thresh = mch_target_thresh
-
-    def fit(self, data, target=None):
-        return self
-
-    def transform(self, data, target=None):
-        data, cols_mv, dropped_cols = mv_col_handler(data, target=self.target, mv_threshold=self.mch_mv_thresh,
-                                                     corr_thresh_features=self.mch_feature_thresh,
-                                                     corr_thresh_target=self.mch_target_thresh)
-
-        print(f'\nFeatures with MV-ratio > {self.mch_mv_thresh}: {len(cols_mv)}')
-        print('Features dropped:', len(dropped_cols), dropped_cols)
-
-        return data
