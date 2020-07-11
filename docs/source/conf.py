c074db28c54f1805376b910ff566806a04ee60b6
Andreas Kanz <Andreas@akanz.de>
2020-07-11 13:03:41 +0200

    path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../..'))
-sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+# sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+sys.path.insert(0, os.path.abspath('../source'))
 # sys.path.insert(0, os.path.abspath('../klib'))
 # sys.path.insert(0, os.path.abspath('../../klib'))
 
