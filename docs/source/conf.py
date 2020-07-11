820290473bfd27422969a1ee08779ee1708673ba
Andreas Kanz <Andreas@akanz.de>
2020-07-11 14:46:33 +0200

    -

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 # sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 # sys.path.insert(0, os.path.abspath('./source'))
 # sys.path.insert(0, os.path.abspath('../klib'))
@@ -33,10 +34,10 @@ release = "0.0.89"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["numpydoc", "sphinx.ext.napoleon", "sphinx.ext.autodoc"]
+extensions = ["numpydoc", "sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,9 +50,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = "default"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
