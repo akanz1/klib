61c11bd66a31d7d62fa6df0e5bf5a3ff83bd5a68
Andreas Kanz <Andreas@akanz.de>
2020-07-11 12:34:13 +0200

    -

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,8 @@
 #
 import os
 import sys
-current_dir = os.path.dirname(__file__)
-target_dir = os.path.abspath(os.path.join(current_dir, "../../klib"))
-sys.path.insert(0, target_dir)
 # sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../klib'))
 
 # -- Project information -----------------------------------------------------
 
