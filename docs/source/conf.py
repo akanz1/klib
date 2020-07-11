818e4411417fed462dac6052c40102b120191cf5
Andreas Kanz <Andreas@akanz.de>
2020-07-11 12:34:13 +0200

    path and deps

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,14 @@
 #
 import os
 import sys
-current_dir = os.path.dirname(__file__)
-target_dir = os.path.abspath(os.path.join(current_dir, "../../klib"))
-sys.path.insert(0, target_dir)
-# sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('../../'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+sys.path.insert(0, os.path.abspath('./source'))
+sys.path.insert(0, os.path.abspath('../klib'))
+sys.path.insert(0, os.path.abspath('../../klib'))
 
 # -- Project information -----------------------------------------------------
 
@@ -33,10 +36,10 @@ release = "0.0.89"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["numpydoc", "sphinx.ext.napoleon"]
+extensions = ["numpydoc", "sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,9 +52,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
