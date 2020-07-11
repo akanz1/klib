32624ecc0910ef5f7547aaa2335e6aa3e58bc1d4
Andreas Kanz <Andreas@akanz.de>
2020-07-11 12:58:16 +0200

    -

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 # sys.path.insert(0, os.path.abspath('../klib'))
 # sys.path.insert(0, os.path.abspath('../../klib'))
 
