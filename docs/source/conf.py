c2b874c310cb90e0a3ea4d6a91ba748c2a0c7089
Andreas Kanz <Andreas@akanz.de>
2020-07-11 14:16:35 +0200

    -

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = "0.0.89"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["numpydoc", "sphinx.ext.napoleon"]
+extensions = ["numpydoc", "sphinx.ext.napoleon", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
