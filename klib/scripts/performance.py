f354507341943662503edca9521aa3f742a8d84a
Andreas Kanz <Andreas@akanz.de>
2021-01-09 16:31:00 +0100

    update performance checks

--- klib/scripts/performance.py
+++ klib/scripts/performance.py
@@ -56,13 +56,13 @@ def time_cat_plot():
 
 def main():
     df_times = pd.DataFrame()
-    df_times["data_cleaning"] = pd.Series([time_data_cleaning() for _ in range(8)])
-    df_times["missingval_plot"] = pd.Series([time_missingval_plot() for _ in range(8)])
-    df_times["dist_plot"] = pd.Series([time_dist_plot() for _ in range(3)])
+    df_times["data_cleaning"] = pd.Series([time_data_cleaning() for _ in range(10)])
+    df_times["missingval_plot"] = pd.Series([time_missingval_plot() for _ in range(6)])
+    df_times["dist_plot"] = pd.Series([time_dist_plot() for _ in range(6)])
     df_times["cat_plot"] = pd.Series([time_cat_plot() for _ in range(8)])
     df_times = df_times.fillna(df_times.mean())
     fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(13, 6))
-    reference_values = [4.3, 6.4, 6, 5.6]
+    reference_values = [4, 8, 8.4, 7]
 
     for i, (col, ref) in enumerate(zip(df_times.columns, reference_values)):
         ax[i].boxplot(df_times[col])
