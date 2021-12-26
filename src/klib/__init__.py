17c8dc2fc279dcc43778c39cede75294db895eea
Andreas Kanz <andreas.kanz@anacision.de>
2021-12-26 13:37:48 +0100

    fix relative imports

--- src/klib/__init__.py
+++ src/klib/__init__.py
@@ -7,8 +7,8 @@ analyzing data.
 
 __author__ = """Andreas Kanz"""
 
-from ._version import __version__
-from .clean import (
+from klib._version import __version__
+from klib.clean import (
     clean_column_names,
     convert_datatypes,
     data_cleaning,
@@ -16,8 +16,13 @@ from .clean import (
     mv_col_handling,
     pool_duplicate_subsets,
 )
-from .describe import cat_plot, corr_mat, corr_plot, dist_plot, missingval_plot
-from .preprocess import cat_pipe, feature_selection_pipe, num_pipe, train_dev_test_split
+from klib.describe import cat_plot, corr_mat, corr_plot, dist_plot, missingval_plot
+from klib.preprocess import (
+    cat_pipe,
+    feature_selection_pipe,
+    num_pipe,
+    train_dev_test_split,
+)
 
 __all__ = [
     "clean_column_names",
