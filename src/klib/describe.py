e8b2fb0e07a92abcd3f163219858b0efef83057a
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-07-23 20:56:27 -0300

    docs(corr_interactive_plot): update docstring

--- src/klib/describe.py
+++ src/klib/describe.py
@@ -439,110 +439,127 @@ def corr_interactive_plot(
     annot: bool = True,
     **kwargs,
 ) -> go.Figure:
-    """Two-dimensional visualization of the correlation between feature-columns
-        using Plotly's Heatmap.
+    """
+    Two-dimensional visualization of the correlation between
+    feature-columns using Plotly's Heatmap.
 
     Parameters
     ----------
     data : pd.DataFrame
-        2D dataset that can be coerced into Pandas DataFrame. If a Pandas DataFrame
-        is provided, the index/column information is used to label the plots
+        2D dataset that can be coerced into a Pandas DataFrame. If a
+        Pandas DataFrame is provided, the index/column information is
+        used to label the plots.
+
     split : Optional[str], optional
-        Type of split to be performed {None, "pos", "neg", "high", "low"}, by default
-        None
-        * None: visualize all correlations between the feature-columns
+        Type of split to be performed
+        {None, "pos", "neg", "high", "low"}, by default None
 
-        * pos: visualize all positive correlations between the feature-columns
-            above the threshold
+        - None: visualize all correlations between the feature-columns
 
-        * neg: visualize all negative correlations between the feature-columns
-            below the threshold
+        - pos: visualize all positive correlations between the
+            feature-columns above the threshold
 
-        * high: visualize all correlations between the feature-columns for
-            which abs (corr) > threshold is True
+        - neg: visualize all negative correlations between the
+            feature-columns below the threshold
 
-        * low: visualize all correlations between the feature-columns for which
-            abs(corr) < threshold is True
+        - high: visualize all correlations between the
+            feature-columns for which abs(corr) > threshold is True
+
+        - low: visualize all correlations between the
+            feature-columns for which abs(corr) < threshold is True
 
     threshold : float, optional
-        Value between 0 and 1 to set the correlation threshold, by default 0 unless
-            split = "high" or split = "low", in which case default is 0.3
+        Value between 0 and 1 to set the correlation threshold,
+        by default 0 unless split = "high" or split = "low", in
+        which case the default is 0.3
+
     target : Optional[pd.Series | str], optional
-        Specify target for correlation. E.g. label column to generate only the
-        correlations between each feature and the label, by default None
+        Specify a target for correlation. For example, the label column
+        to generate only the correlations between each feature and the
+        label, by default None
+
     method : Literal['pearson', 'spearman', 'kendall'], optional
-        method: {"pearson", "spearman", "kendall"}, by default "pearson"
-            * pearson: measures linear relationships and requires normally
-                distributed and homoscedastic data.
-            * spearman: ranked/ordinal correlation, measures monotonic relationships.
-            * kendall: ranked/ordinal correlation, measures monotonic relationships.
-                Computationally more expensive but more robust in smaller dataets
-                than "spearman".
+        Method for correlation calculation:
+        {"pearson", "spearman", "kendall"}, by default "pearson"
+
+        - pearson: measures linear relationships and requires normally
+            distributed and homoscedastic data.
+        - spearman: ranked/ordinal correlation, measures monotonic
+            relationships.
+        - kendall: ranked/ordinal correlation, measures monotonic
+            relationships. Computationally more expensive but more
+            robust in smaller datasets than "spearman".
 
     cmap : str, optional
-        The mapping from data values to color space, matplotlib colormap name or
-        object, or list of colors, by default "BrBG"
+        The mapping from data values to color space, plotly
+        colormap name or object, or list of colors, by default "BrBG"
+
     figsize : tuple[float, float], optional
         Use to control the figure size, by default (12, 10)
+
     annot : bool, optional
         Use to show or hide annotations, by default True
 
     **kwargs : optional
-        Additional elements to control the visualization of the plot. These additional
-        arguments will be passed to the `go.Heatmap` function in Plotly.
+        Additional elements to control the visualization of the plot.
+            These additional arguments will be passed to the `go.Heatmap`
+            function in Plotly.
 
         Specific kwargs used in this function:
 
-        * colorscale: str or list, optional
-            The colorscale to be used for the heatmap. It controls the mapping of data
-            values to colors in the heatmap.
+        - colorscale: str or list, optional
+            The colorscale to be used for the heatmap. It controls the
+            mapping of data values to colors in the heatmap.
 
-        * zmax: float, optional
-            The maximum value of the color scale. It limits the upper range of the
-            colorbar displayed on the heatmap.
+        - zmax: float, optional
+            The maximum value of the color scale. It limits the upper
+            range of the colorbar displayed on the heatmap.
 
-        * zmin: float, optional
-            The minimum value of the color scale. It limits the lower range of the
-            colorbar displayed on the heatmap.
+        - zmin: float, optional
+            The minimum value of the color scale. It limits the lower
+            range of the colorbar displayed on the heatmap.
 
-        * text: pd.DataFrame, optional
-            A DataFrame containing text to display on the heatmap. This text will be
-            shown on the heatmap cells corresponding to the correlation values.
+        - text: pd.DataFrame, optional
+            A DataFrame containing text to display on the heatmap. This
+            text will be shown on the heatmap cells corresponding to the
+            correlation values.
 
-        * texttemplate: str, optional
-            A text template string to format the text display on the heatmap. This allows
-            you to customize how the text appears, including the display of the correlation
-            values.
+        - texttemplate: str, optional
+            A text template string to format the text display on the
+            heatmap. This allows you to customize how the text appears,
+            including the display of the correlation values.
 
-        * textfont: dict, optional
-            A dictionary specifying the font properties for the text on the heatmap. You
-            can customize the font size, color, family, etc., for the text annotations.
+        - textfont: dict, optional
+            A dictionary specifying the font properties for the text on
+            the heatmap. You can customize the font size, color, family,
+            etc., for the text annotations.
 
-        * x: list, optional
-            The list of column names for the x-axis of the heatmap. It allows you to
-            customize the labels displayed on the x-axis.
+        - x: list, optional
+            The list of column names for the x-axis of the heatmap. It
+            allows you to customize the labels displayed on the x-axis.
 
-        * y: list, optional
-            The list of row names for the y-axis of the heatmap. It allows you to
-            customize the labels displayed on the y-axis.
+        - y: list, optional
+            The list of row names for the y-axis of the heatmap. It
+            allows you to customize the labels displayed on the y-axis.
 
-        * z: pd.DataFrame, optional
-            The 2D array representing the correlation matrix to be visualized. This is the
-            core data for generating the heatmap, containing the correlation values.
+        - z: pd.DataFrame, optional
+            The 2D array representing the correlation matrix to be
+            visualized. This is the core data for generating the heatmap,
+            containing the correlation values.
 
-        * Many more kwargs are available, e.g., "hovertemplate" to control the legend
-            hover template, or options to adjust the borderwidth and opacity of the heatmap.
-            For a comprehensive list of available kwargs, please refer to the Plotly
-            Heatmap documentation.
+        - Many more kwargs are available, e.g., "hovertemplate" to control
+            the legend hover template, or options to adjust the borderwidth
+            and opacity of the heatmap. For a comprehensive list of
+            available kwargs, please refer to the Plotly Heatmap documentation.
 
-        Kwargs can be supplied through a dictionary of key-value pairs (see above) and
-        can be found in Plotly Heatmap documentation.
+        Kwargs can be supplied through a dictionary of key-value pairs
+        (see above) and can be found in Plotly Heatmap documentation.
 
     Returns
     -------
     heatmap : plotly.graph_objs._figure.Figure
-        A Plotly Figure object representing the heatmap visualization of feature
-        correlations.
+        A Plotly Figure object representing the heatmap visualization of
+        feature correlations.
     """
     # Validate Inputs
     _validate_input_range(threshold, "threshold", -1, 1)
